f6d5c9f1fcf390edce00bda9605e172aa61e8957
Danielle Silva de Melo <danielleesilva.4@gmail.com>
2025-10-21 14:24:28 -0400

    add data-test

--- src/components/MetricCard.tsx
+++ src/components/MetricCard.tsx
@@ -3,7 +3,7 @@ import type { MetricCardProps } from "@/types/admin";
 
 export function MetricCard({ title, value, icon }: MetricCardProps) {
   return (
-    <div className={`bg-[var(--global-text-secondary)] rounded-md p-6  hover:shadow-md transition-shadow`}>
+    <div className={`bg-[var(--global-text-secondary)] rounded-md p-6  hover:shadow-md transition-shadow`} data-test="metric-card">
       <div className="flex items-center justify-between">
         <div>
           <p className="text-sm font-medium text-[var(--global-bg)] mb-1">{title}</p>
